ad7e69b0df2472f87bf95fb10effe9772f022f5c
Joel Papineau <118376438+j-papineau@users.noreply.github.com>
2023-06-28 14:41:14 -0400

    Initial Commit

---
+++ EgoLyfter/EgoLyfterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgoLyfterApp.swift
+//  EgoLyfter
+//
+//  Created by Joel on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct EgoLyfterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
